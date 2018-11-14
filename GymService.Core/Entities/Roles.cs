3706a8952972c11cb5f5d6cb1efd6c121e248d44
Rafał Panasewicz <rpanasewicz@gmail.com>
2018-11-14 20:52:38 +0100

    User entity impl

---
+++ GymService.Core/Entities/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymService.Core.Entities
+{
+    public class Roles
+    {
+        public static string User => "user";
+        public static string Trainer => "trainer";
+        public static string Guest => "guest";
+        public static string Receptionist => "receptionist";
+    }
+}
---
+++ GymService.Core/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymService.Core.Entities
+{
+    public class User
+    {
+        public Guid Id { get; protected set; }
+        public string Email { get; protected set; }
+        public string Password { get; protected set; }
+        public string Salt { get; protected set; }
+        public string Username { get; protected set; }
+        public string FullName { get; protected set; }
+        public string Role { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+    }
+}

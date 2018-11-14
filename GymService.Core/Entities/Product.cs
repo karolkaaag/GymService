d0bae13328af5bcc070eade28dfadc721b98aa6b
Rafał Panasewicz <rpanasewicz@gmail.com>
2018-11-14 21:13:53 +0100

    GymClass and Product imp

---
+++ GymService.Core/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymService.Core.Entities
+{
+    class Product
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public double BasePrice { get; protected set; }
+    }
+}
